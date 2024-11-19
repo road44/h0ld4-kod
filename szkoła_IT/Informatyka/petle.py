256bcad0694fa6b702f7cafb823f2c0304947c31
h0ld4 <52355930+road44@users.noreply.github.com>
2024-11-19 11:18:57 +0100

    asjfaghkja

---
+++ szkoła_IT/Informatyka/petle.py
@@ -0,0 +1,43 @@
+'''składnia pętli while
+
+while warunek:
+    instrukcje, gry war. jest spełniony 
+    inkrementacja(zwiększenie o 1)'''
+
+# wyświetla zmienną i dopóki będzie <10
+pauza="--------------------"
+i=1
+# nigdy w warunku jest równe
+# tylko mniejsze bądz równe
+while i<10:
+    print(i)
+    i+=1
+
+print(pauza)
+
+# 10-20 co 2
+
+a=10
+while a<=20:
+    print(a)
+    a+=2
+
+print(pauza)
+
+# od 100 do 0
+z=100
+
+while z>=0:
+    print(z, end=" ")
+    z-=10
+print(pauza)
+
+print(pauza)
+
+
+'''Pętla FOR
+
+for i in range(zakres):
+    instrukcje'''
+
+# for 1-10
