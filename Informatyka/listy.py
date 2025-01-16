b6aaa36eca1aff5dc030ea6997672d1eee742c8f
h0ld4 <52355930+road44@users.noreply.github.com>
2025-01-16 09:16:57 +0100

    listy wersja pierwsza

---
+++ Informatyka/listy.py
@@ -0,0 +1,3 @@
+'''lista to zbiór elementów zapisanych w kwadratowych nawiasach [] pod jedną nazwą zmiennej np. lista = [1, 2, 3, 4, 5]. 
+Elementy mogą być różnego typu oddzielone przecinkami, mogą się powtarzać.
+aby odwołać się do konkretnego elementu z listy korzystamy z możliwości indeksowania elementów. (pierwszy element ma indeks 0)'''
