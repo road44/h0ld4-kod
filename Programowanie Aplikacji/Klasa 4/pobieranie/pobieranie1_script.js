2cfbf9e16649dae878877c17dd4e75a32f794830
h0ld4 <52355930+road44@users.noreply.github.com>
2025-09-05 11:05:13 +0200

    1.5 (js)

--- Programowanie Aplikacji/Klasa 4/pobieranie/pobieranie1_script.js
+++ Programowanie Aplikacji/Klasa 4/pobieranie/pobieranie1_script.js
@@ -1,5 +1,6 @@
 // getElementById()- odwołanie do konkretnego id
-
+let test1 = document.getElementById("cos");
+console.log(test1);
 // getElementsByTagName()- odwołanie do znacznika html (kolekcja html)
 
 // getElementsByClassName()- odwołanie do klasy
