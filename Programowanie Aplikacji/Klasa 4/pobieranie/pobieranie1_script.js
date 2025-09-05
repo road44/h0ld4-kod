70a1d3a4c4a7fc7df01144a03f268298bd9f174f
h0ld4 <52355930+road44@users.noreply.github.com>
2025-09-05 11:11:12 +0200

    1.6 (js)

--- Programowanie Aplikacji/Klasa 4/pobieranie/pobieranie1_script.js
+++ Programowanie Aplikacji/Klasa 4/pobieranie/pobieranie1_script.js
@@ -2,5 +2,6 @@
 let test1 = document.getElementById("cos");
 console.log(test1);
 // getElementsByTagName()- odwołanie do znacznika html (kolekcja html)
-
+let test2 = document.getElementsByTagName("li");
+console.log(test2);
 // getElementsByClassName()- odwołanie do klasy
