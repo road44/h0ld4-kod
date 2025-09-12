1e5149a20849529827fcadef93eee181765df8f4
h0ld4 <52355930+road44@users.noreply.github.com>
2025-09-12 08:38:40 +0200

    2.3

--- Programowanie Aplikacji/Klasa 4/pobieranie/pobieranie2_script.js
+++ Programowanie Aplikacji/Klasa 4/pobieranie/pobieranie2_script.js
@@ -7,3 +7,15 @@ console.log(zm2);
 // odwołanie do linków wersja 2
 const zm3 = document.querySelectorAll("a");
 console.log(zm3);
+// odwołanie do wszystkich spanów
+const zm4 = document.querySelectorAll("span");
+console.log(zm4);
+// odwołanie do spana w divie
+const zm5 = document.querySelector("div span");
+console.log(zm5);
+// odwołanie do li
+const zm6 = document.querySelectorAll("li");
+console.log(zm6);
+// odwołanie do li w ul
+const zm7 = document.querySelectorAll("ul li");
+console.log(zm7);
