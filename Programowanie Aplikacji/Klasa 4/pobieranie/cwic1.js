e96dff82a35bddbb0a822321de340205f5c7ccd6
h0ld4 <52355930+road44@users.noreply.github.com>
2025-09-12 09:16:51 +0200

    cw 1.3

--- Programowanie Aplikacji/Klasa 4/pobieranie/cwic1.js
+++ Programowanie Aplikacji/Klasa 4/pobieranie/cwic1.js
@@ -1,1 +1,14 @@
-const ullist=document.createElement('ul')
+// tworze ul
+const ullist = document.createElement("ul");
+// dodaje ul do sekcji body
+
+document.body.appendChild(ullist)
+
+for(let i=1; i<=10; i++){
+// tu właśnie tworze li
+
+
+
+
+
+}
