09d20f8e9ed392c633fa1f4b159a0a3200b33db4
h0ld4 <52355930+road44@users.noreply.github.com>
2025-09-12 08:32:30 +0200

    15 + 2.2

--- Programowanie Aplikacji/Klasa 4/pobieranie/pobieranie2_script.js
+++ Programowanie Aplikacji/Klasa 4/pobieranie/pobieranie2_script.js
@@ -1,4 +1,9 @@
 // odwołanie do id pusty
 const zm1 = document.getElementById("pusty");
 console.log(zm1);
-// odwołanie do linków
+// odwołanie do linków wersja 1
+const zm2 = document.getElementsByTagName("a");
+console.log(zm2);
+// odwołanie do linków wersja 2
+const zm3 = document.querySelectorAll("a");
+console.log(zm3);
