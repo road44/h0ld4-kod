1af9a8548dd6e7d11f1813686c54a2b2dec6f3f8
h0ld4 <52355930+road44@users.noreply.github.com>
2025-09-12 11:02:15 +0200

    1.3

--- Programowanie Aplikacji/Klasa 4/pobieranie/dodaw.js
+++ Programowanie Aplikacji/Klasa 4/pobieranie/dodaw.js
@@ -1,3 +1,12 @@
 // tworze ul liste
-const ul = document.createElement("ul");
+const ulList = document.createElement("ul");
 const liItem = document.createElement("li");
+
+// dodaje liste do sekcji body
+
+document.body.appendChild(ulList);
+
+// dodaje text do li
+
+liItem.textContent = "to jest podpunkt listy";
+ulList.appendChild(liItem)
