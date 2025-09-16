7f823a578dc982685478b19b91876ed2f1b8a2e6
h0ld4 <52355930+road44@users.noreply.github.com>
2025-09-16 16:33:34 +0200

    js1

---
+++ Programowanie Aplikacji/Klasa 4/pobieranie/cwicz2.js
@@ -0,0 +1,5 @@
+const btn1 = document.querySelector(".btn1");
+const btn2 = document.querySelector(".btn2");
+const squaer = document.querySelector(".square");
+const p1 = document.querySelector(".p1");
+const p2 = document.querySelector(".p2");
