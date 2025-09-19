e8fab0cabb0ca1d0278f646b4bebad29b728636f
h0ld4 <52355930+road44@users.noreply.github.com>
2025-09-19 09:27:21 +0200

    1.3

--- Programowanie Aplikacji/Klasa 4/pobieranie/add-event/addevent.js
+++ Programowanie Aplikacji/Klasa 4/pobieranie/add-event/addevent.js
@@ -8,6 +8,10 @@ const moja1 = () => {
 	console.log("btn1 klikniety");
 };
 
+const moja2 = () => {
+	console.log("najechano na btn2");
+};
 // dodaje listenera
 
 btn1.addEventListener("click", moja1);
+btn2.addEventListener("mouseenter", moja2);
