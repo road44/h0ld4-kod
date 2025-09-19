4ecb9721d2bd2e39e5daa64cf788a5c4f27ad4dc
h0ld4 <52355930+road44@users.noreply.github.com>
2025-09-19 09:40:44 +0200

    1.5

--- Programowanie Aplikacji/Klasa 4/pobieranie/add-event/addevent.js
+++ Programowanie Aplikacji/Klasa 4/pobieranie/add-event/addevent.js
@@ -1,22 +1,32 @@
 const btn1 = document.querySelector(".btn1");
 const btn2 = document.querySelector(".btn2");
 const btn3 = document.querySelector(".btn3");
-
-// funckja 1
+const allbtn = document.querySelector(".btn");
+// kliknięcie
 
 const moja1 = () => {
 	console.log("btn1 klikniety");
 };
-
+// najechanie
 const moja2 = () => {
 	console.log("najechano na btn2");
 };
-
+// podwójne kliknięcie
 const moja3 = () => {
 	console.log("🤠");
 };
+
+// funckja smile przechowa 3 buttony
+// const smile = () => {
+// 	console.log("😶‍🌫️");
+// };
+
 // dodaje listenera
 
 btn1.addEventListener("click", moja1);
 btn2.addEventListener("mouseenter", moja2);
 btn3.addEventListener("dblclick", moja3);
+// allbtn.forEach((btn) => btn.addEventListener("click", smile));
+// allbtn.array.forEach((btn) => {
+// 	btn.addEventListener("click", smile);
+// });
