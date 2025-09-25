4ff4eb53cc048f17bacccac4b83c512194407317
h0ld4 <52355930+road44@users.noreply.github.com>
2025-09-25 12:01:06 +0200

    1.1

---
+++ Programowanie Aplikacji/Klasa 4/pobieranie/zwiekszanie/zwi.js
@@ -0,0 +1,4 @@
+let plus = document.querySelectorAll("sizeUp");
+let minus = document.querySelectorAll(".sizeDown");
+let color = document.querySelectorAll(".color");
+let p = document.querySelectorAll("p");
