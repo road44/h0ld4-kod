0b7c3a00187d819548cc84966af10e6f8a584df7
h0ld4 <52355930+road44@users.noreply.github.com>
2025-09-25 12:23:54 +0200

    gotowe

--- Programowanie Aplikacji/Klasa 4/pobieranie/zwiekszanie/zwi.js
+++ Programowanie Aplikacji/Klasa 4/pobieranie/zwiekszanie/zwi.js
@@ -1,4 +1,40 @@
-let plus = document.querySelectorAll("sizeUp");
-let minus = document.querySelectorAll(".sizeDown");
-let color = document.querySelectorAll(".color");
-let p = document.querySelectorAll("p");
+let sizeUp = document.querySelector(".sizeUp");
+let sizeDown = document.querySelector(".sizeDown");
+let colorBtn = document.querySelector(".color");
+let p = document.querySelector("p");
+
+let fontSize = 30;
+
+// const upper = () => {
+// 	if (fontSize >= 90) {
+// 		return;
+// 	}
+// 	p.style.fontSize = fontSize + "px";
+// 	fontSize += 5;
+// };
+const upper = () => {
+	if (fontSize >= 90) {
+		return;
+	}
+	p.style.fontSize = fontSize + "px";
+	fontSize += 5;
+};
+
+const lower = () => {
+	if (fontSize <= 12) {
+		return;
+	}
+	fontSize -= 5;
+	p.style.fontSize = fontSize + "px";
+};
+
+const colorChange = (param) => {
+	const r = Math.floor(Math.random() * 255);
+	const g = Math.floor(Math.random() * 255);
+	const b = Math.floor(Math.random() * 255);
+	p.style.color = `rgb(${r},${g},${b})`;
+};
+
+sizeUp.addEventListener("click", upper);
+sizeDown.addEventListener("click", lower);
+colorBtn.addEventListener("click", colorChange);
