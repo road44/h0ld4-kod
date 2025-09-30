938278fdb4363090e5fe48ea77025fd3a029e3f5
h0ld4 <52355930+road44@users.noreply.github.com>
2025-09-30 16:25:55 +0200

    2.3

--- Programowanie Aplikacji/Klasa 4/konwerter_html/konwerter.js
+++ Programowanie Aplikacji/Klasa 4/konwerter_html/konwerter.js
@@ -6,3 +6,11 @@ const result = document.querySelector(".result");
 const convBtn = document.querySelector(".conv");
 const resetBtn = document.querySelector(".reset");
 const changeBtn = document.querySelector(".change");
+// t(f)=t(c)*1.8+32
+// t(c)=(t(f)-32)/1.8
+
+let fahr;
+let cels;
+
+// funkcja swap zmienia c ja f lub odwrotnie
+// trzbeba sprwadzić co jest w spanie one i na tej podstawie zamienić
