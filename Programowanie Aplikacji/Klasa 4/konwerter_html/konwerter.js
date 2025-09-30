0320edabae2e583006eb16e57f998510d9819a12
h0ld4 <52355930+road44@users.noreply.github.com>
2025-09-30 16:22:09 +0200

    2.2

--- Programowanie Aplikacji/Klasa 4/konwerter_html/konwerter.js
+++ Programowanie Aplikacji/Klasa 4/konwerter_html/konwerter.js
@@ -2,3 +2,7 @@ const one = document.querySelector(".one");
 const two = document.querySelector(".two");
 
 const converter = document.querySelector("#converter");
+const result = document.querySelector(".result");
+const convBtn = document.querySelector(".conv");
+const resetBtn = document.querySelector(".reset");
+const changeBtn = document.querySelector(".change");
