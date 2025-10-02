ef13ec0203fbde683381eda08b270aa4c1a455e0
h0ld4 <52355930+road44@users.noreply.github.com>
2025-10-02 12:27:38 +0200

    finale

--- Programowanie Aplikacji/Klasa 4/konwerter_html/konwerter.js
+++ Programowanie Aplikacji/Klasa 4/konwerter_html/konwerter.js
@@ -28,14 +28,38 @@ const swap = () => {
 const celToFahr = () => {
 	fahr = converter.value * 1.8 + 32;
 	result.textContent = `${converter.value}°C to ${fahr.toFixed(1)}°F`;
+	converter.value = "";
 };
 
 // t(c)=(t(f)-32)/1.8
 const FahrtoCel = () => {
 	cels = (converter.value - 32) / 1.8;
-	result.textContent = `${converter.value}°F to ${cels.toFixed(1)}`;
+	result.textContent = `${converter.value}°F to ${cels.toFixed(1)}°C`;
+	converter.value = "";
 };
+
+// aby móc obsługiwać obie funkcje jednym przyciskiem dodam nową
+
+const conversion = () => {
+	if (converter.value != "") {
+		if (one.textContent === "°C") {
+			celToFahr();
+		} else {
+			FahrtoCel();
+		}
+	} else {
+		result.textContent = `Wpisz jakąś wartość`;
+	}
+};
+
+// funkcja reset
+const resetAll = () => {
+	result.textContent = "";
+	converter.value = "";
+};
+
 // dodajemy listenera
 
 changeBtn.addEventListener("click", swap);
-convBtn.addEventListener("click", celToFahr);
+convBtn.addEventListener("click", conversion);
+resetBtn.addEventListener("click", resetAll);
