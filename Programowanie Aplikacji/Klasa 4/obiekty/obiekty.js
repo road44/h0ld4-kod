61a00ba7e03ce8f61fb04cc8ed4a55432ca91565
h0ld4 <52355930+road44@users.noreply.github.com>
2025-10-07 15:47:28 +0200

    1.1

---
+++ Programowanie Aplikacji/Klasa 4/obiekty/obiekty.js
@@ -0,0 +1,38 @@
+// składnia obiektu
+// const nameObj = {
+//     key:value,
+//     key:value,
+//     key:value,
+// }
+// każdy obiekt składa sie z pary klucz: wartosc
+let pauza = "------------------";
+const user = {
+	name: "adam",
+	age: 25,
+};
+
+console.log(user);
+console.log(user.name);
+//
+console.log(pauza);
+// pętla for in
+for (let klucz in user) {
+	console.log(klucz + ":" + user[klucz]);
+}
+console.log(pauza);
+
+// obiekt w obiekcie
+
+const newUser = {
+	name: "adam",
+	age: 25,
+	car: {
+		brand: "audi",
+		model: "a3",
+		kolor: "czarny",
+	},
+};
+
+console.log(newUser);
+console.log(newUser.car);
+console.log(pauza);
