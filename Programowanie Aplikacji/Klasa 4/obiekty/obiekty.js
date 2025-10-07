f545ee5accad6e3df9fc871669165685eb210ef4
h0ld4 <52355930+road44@users.noreply.github.com>
2025-10-07 15:57:37 +0200

    1.3

--- Programowanie Aplikacji/Klasa 4/obiekty/obiekty.js
+++ Programowanie Aplikacji/Klasa 4/obiekty/obiekty.js
@@ -37,3 +37,22 @@ console.log(newUser);
 console.log(newUser.car);
 console.log(newUser.car.model);
 console.log(pauza);
+// <adam> jeździ <kolor> samochodem marki <audi>
+console.log(
+	`${newUser.name} jeździ ${newUser.car.kolor} samochodem marki ${newUser.car.brand}`
+);
+
+const myuser = {
+	imie: "ewa",
+	wiek: 18,
+	car: {
+		brand: "reno",
+		model: "twingo",
+		color: "srebrny",
+		sound: function () {
+			console.log(`brum, brum....`);
+		},
+	},
+};
+// odowałenie do metody
+myuser.car.sound();
