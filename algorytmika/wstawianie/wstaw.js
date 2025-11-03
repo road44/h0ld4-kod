490335a42ac6ed312bd9834d4ed1895f46743566
h0ld4 <52355930+road44@users.noreply.github.com>
2025-11-03 15:05:29 +0100

    1.3

--- algorytmika/wstawianie/wstaw.js
+++ algorytmika/wstawianie/wstaw.js
@@ -1,20 +1,20 @@
 // Funkcja realizująca sortowanie przez wstawianie
-function insertionSort(arr) {
+function wstawSort(arr) {
     // Przechodzimy przez tablicę zaczynając od drugiego elementu
     for (let i = 1; i < arr.length; i++) {
         // Zapamiętujemy aktualny element do wstawienia
-        let currentElement = arr[i];
+        let obEle = arr[i];
         // Indeks poprzedniego elementu
         let j = i - 1;
 
         // Przesuwamy elementy większe od currentElement o jedną pozycję w prawo
-        while (j >= 0 && arr[j] > currentElement) {
+        while (j >= 0 && arr[j] > obEle) {
             arr[j + 1] = arr[j];
             j--;
         }
 
         // Wstawiamy element w odpowiednie miejsce
-        arr[j + 1] = currentElement;
+        arr[j + 1] = obEle;
     }
     return arr;
 }
@@ -22,5 +22,5 @@ function insertionSort(arr) {
 // Przykładowa tablica do posortowania
 let array = [-1, 23, -2, 0.9, 0.3];
 console.log("Lista:", array);
-insertionSort(array);
+wstawSort(array);
 console.log("Lista posortowana:", array);
