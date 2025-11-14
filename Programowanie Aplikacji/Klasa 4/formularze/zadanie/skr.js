0ddd10bb2a253f3f22a9dc1a0298dd02aa6a25b8
road44 <52355930+road44@users.noreply.github.com>
2025-11-14 10:47:35 +0100

    1.2

---
+++ Programowanie Aplikacji/Klasa 4/formularze/zadanie/skr.js
@@ -0,0 +1,25 @@
+let zgoda = document.querySelector("#zgoda");
+let nazwisko = document.querySelector("#nazwisko").value;
+let szkola = document.querySelector("#szkola").value;
+let kodRab = document.querySelector("#kod-rab").value;
+let rabBtn = document.querySelector("#rab-btn");
+let czysc = document.querySelector("#cl");
+
+console.log(nazwisko);
+/*
+
+let rabatWz = /^[0]{1}\/[f]{1}[r]{1}[e]{2}\s[0-9]{2}$/i;
+let szkolaWz = /^ZSPM[0-10]{1,4}$/i;
+function sprawdz() {
+	if (zgoda.checked) {
+		if (nazwisko != "" && szkola.match(szkolaWz) && kodRab.match(rabatWz)) {
+			document.write("Witaj w strefie bezpłatnych szkoleń Akademi Cisco");
+		} else {
+			document.write("Błąd w danych");
+			document.write(`<br> ${nazwisko} oraz ${szkola} oraz ${kodRab}`);
+		}
+	} else {
+		document.write("Musisz wyrazić zgode");
+	}
+}
+    */
