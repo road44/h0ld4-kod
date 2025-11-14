2fbdea55b6d1791dd8e13c89d2b0b28e3c8d7dfd
h0ld4 <filipo.tab@gmail.com>
2025-11-14 13:19:58 +0100

    1.3

--- Programowanie Aplikacji/Klasa 4/formularze/zadanie/skr.js
+++ Programowanie Aplikacji/Klasa 4/formularze/zadanie/skr.js
@@ -1,16 +1,16 @@
 let zgoda = document.querySelector("#zgoda");
-let nazwisko = document.querySelector("#nazwisko").value;
-let szkola = document.querySelector("#szkola").value;
-let kodRab = document.querySelector("#kod-rab").value;
+
 let rabBtn = document.querySelector("#rab-btn");
 let czysc = document.querySelector("#cl");
 
 console.log(nazwisko);
-/*
 
-let rabatWz = /^[0]{1}\/[f]{1}[r]{1}[e]{2}\s[0-9]{2}$/i;
-let szkolaWz = /^ZSPM[0-10]{1,4}$/i;
+let rabatWz = /^0\/free\s[0-9]{2}$/i;
+let szkolaWz = /^ZSPM$/i;
 function sprawdz() {
+	let nazwisko = document.querySelector("#nazwisko").value;
+	let szkola = document.querySelector("#szkola").value;
+	let kodRab = document.querySelector("#kod-rab").value;
 	if (zgoda.checked) {
 		if (nazwisko != "" && szkola.match(szkolaWz) && kodRab.match(rabatWz)) {
 			document.write("Witaj w strefie bezpłatnych szkoleń Akademi Cisco");
@@ -22,4 +22,3 @@ function sprawdz() {
 		document.write("Musisz wyrazić zgode");
 	}
 }
-    */
