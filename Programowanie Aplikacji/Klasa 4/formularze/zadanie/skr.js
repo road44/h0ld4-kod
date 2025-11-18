8d38fbfd141419bb00e4db65183fed899903fd2e
h0ld4 <52355930+road44@users.noreply.github.com>
2025-11-18 21:55:23 +0100

    Update skr.js

--- Programowanie Aplikacji/Klasa 4/formularze/zadanie/skr.js
+++ Programowanie Aplikacji/Klasa 4/formularze/zadanie/skr.js
@@ -3,7 +3,6 @@ let zgoda = document.querySelector("#zgoda");
 let rabBtn = document.querySelector("#rab-btn");
 let czysc = document.querySelector("#cl");
 
-console.log(nazwisko);
 
 let rabatWz = /^0\/free\s[0-9]{2}$/i;
 let szkolaWz = /^ZSPM$/i;
@@ -16,7 +15,6 @@ function sprawdz() {
 			document.write("Witaj w strefie bezpłatnych szkoleń Akademi Cisco");
 		} else {
 			document.write("Błąd w danych");
-			document.write(`<br> ${nazwisko} oraz ${szkola} oraz ${kodRab}`);
 		}
 	} else {
 		document.write("Musisz wyrazić zgode");
