4234b7bca8eb666beee6e98eb96975b0752d384c
road44 <52355930+road44@users.noreply.github.com>
2025-12-18 11:56:40 +0100

    php 2

--- Programowanie Aplikacji/Klasa 4/PHP/index.php
+++ Programowanie Aplikacji/Klasa 4/PHP/index.php
@@ -4,26 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>wstep</title>
 </head>
 
 <body>
-<?php
-echo "pierwsze kroki w php";
-print("<br>instrukcja PRINT może być zamiennie używana z echo");
-echo "<br><strong><i>tekst sformatowany</i></strong>";
 
 
-
-echo "<br> znak kropki kropki to konkatenacja łańcuchów znakowych <br> <br>";
-$im1='kasia';
-$im2 = 'filip';
-$im3 = 'kuba';
-
-echo $im1. ", " .$im2. ", " .$im3;
-
-
-?>
+<br>
+<a href="zad1.php">zadanie 1</a>
 
 </body>
 
