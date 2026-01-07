cc732a9e94d1bbf137cadb11acfd821836cae1f2
road44 <52355930+road44@users.noreply.github.com>
2026-01-07 09:05:33 +0100

    1.1

--- Programowanie Aplikacji/Klasa 4/PHP/zadania_php/tekstowe.php
+++ Programowanie Aplikacji/Klasa 4/PHP/zadania_php/tekstowe.php
@@ -1,19 +1,11 @@
 <!DOCTYPE html>
 <html lang="pl">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>wstep</title>
+    <title>Document</title>
 </head>
-
 <body>
-
-
-<br>
-<a href="zadania_php/zad1.php">zadanie 1</a> <br>
-<a href="zadania_php/tekstowe.php">tekstowe</a>
-
+    
 </body>
-
 </html>
