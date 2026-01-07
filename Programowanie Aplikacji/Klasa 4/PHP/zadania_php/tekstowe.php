bde856f7eb5dba9446c7db8e289fb9d2343a31fe
road44 <52355930+road44@users.noreply.github.com>
2026-01-07 09:33:09 +0100

    1.3

--- Programowanie Aplikacji/Klasa 4/PHP/zadania_php/tekstowe.php
+++ Programowanie Aplikacji/Klasa 4/PHP/zadania_php/tekstowe.php
@@ -1,14 +1,32 @@
 <?php
 
-$txt="Rozpoczynamy nauke";
+$txt="Rozpoczynamy nauke PHP.";
 
 $dl=strlen($txt);
 
-echo "Długość naszego ciągu wynosi $dl";
+echo "Długość naszego ciągu wynosi $dl znaków.";
 
 
+$pierwsza=substr($txt,0,1) ;
 
+echo "<br> Pierwsza litera ze zdania to $pierwsza";
 
+$kawalek=substr($txt,19,3 );
+
+echo"<br> $kawalek";
+
+$pauza="<br>";
+
+echo $pauza;
+
+$wielkie=strtoupper($txt);
+echo"Zamiana na wielkie litery $wielkie";
+$male=strtolower($txt);
+echo "$pauza Zamiana na małe litery: $male";
+
+$odwrocony=strrev($txt);
+
+echo "$pauza Tekst od tyłu: $odwrocony";
 
 
 ?>
