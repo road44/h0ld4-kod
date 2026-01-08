e3ba0f72f09d1bc4265fd48a9d04301d32f02ded
road44 <52355930+road44@users.noreply.github.com>
2026-01-08 08:28:36 +0100

    1.2

--- Programowanie Aplikacji/Klasa 4/PHP/zadania_php/funk_mat.php
+++ Programowanie Aplikacji/Klasa 4/PHP/zadania_php/funk_mat.php
@@ -2,5 +2,23 @@
 
 $pauza = "<br>";
 
+$a=15;
+$cos ="12";
+$b = 3.75;
+$liczby=[457,0,-5,789,4,-99,1,579];
+
+echo gettype($a);
+echo $pauza;
+echo gettype($b);
+echo $pauza;
+echo is_float($b);
+echo $pauza;
+echo is_numeric($b);
+echo $pauza;
+echo isset($x); //nic nie zwraca
+echo $pauza;
+var_dump($a);
+echo $pauza;
+
 
 ?>
