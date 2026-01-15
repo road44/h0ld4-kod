c64f523cb5a01ad4de1308eb904404e89b26133f
road44 <52355930+road44@users.noreply.github.com>
2026-01-15 09:12:18 +0100

    1.2

--- Programowanie-Aplikacji/Klasa-4/PHP/zadania_php/rzutowanie.php
+++ Programowanie-Aplikacji/Klasa-4/PHP/zadania_php/rzutowanie.php
@@ -1,1 +1,28 @@
-<!-- rzutowanie polega na zmianie typu danych/zmiennych -->
+<!-- rzutowanie polega na zmianie typu danych/zmiennychrzutowanie-lekcja -->
+
+
+<?php
+
+$pauza = "<br>";
+// przykład 1
+$liczba=12.75;
+
+echo (int)$liczba;
+echo $pauza;
+
+// przykład 2
+
+$x=10;
+$wynik1=(float)$x;
+$wynik2=(string)$x;
+
+echo $wynik1 . $pauza;
+// echo $pauza . $pauza;
+echo $wynik2 . $pauza;
+echo $pauza;
+echo gettype($wynik1) . $pauza;
+echo gettype($wynik2) . $pauza;
+
+echo $pauza;
+
+?>
