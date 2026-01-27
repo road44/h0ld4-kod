3dfad85039ce021dd0c5ce3da7925d5a27ac4dc2
road44 <52355930+road44@users.noreply.github.com>
2026-01-27 13:13:56 +0100

    1.2

--- Programowanie-Aplikacji/Klasa-4/PHP/zadania_php/cwiczenie.php
+++ Programowanie-Aplikacji/Klasa-4/PHP/zadania_php/cwiczenie.php
@@ -1,5 +1,35 @@
 <?php
+$pauza = "<br> <br>";
+
+echo "rozpoczynamy nauke w <b>PHP</b>";
+echo $pauza;
 
 
+echo "pierwsza linijka <br> druga linijka tekstu";
+
+echo $pauza;
+
+echo "Typy danych w php <br>";
+echo '<ul style="list-style-type: square;"><li>intiger</li><li>float</li><li>double</li><li>string</li></ul>';
+
+
+$a = 10;
+$b = 5;
+$c = ($a - $b) * 2;
+
+echo "$c";
+echo $pauza;
+echo '<span style="color:blue;">Pamiętaj. PHP zna kolejność wykonywania działań</span>';
+echo $pauza;
+
+$x = 4;
+$y = 4;
+$z =4;
+$w =4;
+
+echo 'Wartość zmiennej $x wynosi ' . $x +=3;
+echo '<br>Wartość zmiennej $y wynosi ' . $y -+2;
+echo '<br> Wartość zmiennej $z wynosi ' . $z *=2;
+echo '<br> Wartość zmiennej $w wynosi ' . $w /= 3;
 
 ?>
