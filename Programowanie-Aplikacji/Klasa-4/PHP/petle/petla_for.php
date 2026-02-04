ba5aa83c0ad4f0febbacfaf239bd385500d48bae
road44 <52355930+road44@users.noreply.github.com>
2026-02-04 09:40:10 +0100

    1.4(while)

--- Programowanie-Aplikacji/Klasa-4/PHP/petle/petla_for.php
+++ Programowanie-Aplikacji/Klasa-4/PHP/petle/petla_for.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>petla for</title>
 </head>
 <body>
     <?php
@@ -30,8 +30,19 @@ while($c<=6){
     echo "Kasia ";
     $c++;
 }
+echo $linia;
+$d = 0;
+while($d<=10){
+    echo "$d ";
+    $d++;
+}
 
-
+echo $pauza;
+$e =0;
+while($e++<=10){
+    // $e++;
+    echo "$e ";
+}
 ?>
 </body>
 </html>
