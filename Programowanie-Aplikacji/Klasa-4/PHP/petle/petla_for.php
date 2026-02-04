d53fa2bb2ef41f26277a5dca2544efb610adb775
road44 <52355930+road44@users.noreply.github.com>
2026-02-04 09:35:05 +0100

    1.2 (for)

---
+++ Programowanie-Aplikacji/Klasa-4/PHP/petle/petla_for.php
@@ -0,0 +1,20 @@
+<?php
+
+$pauza = "<br>";
+$linia = "<hr>";
+$oba = "<br><hr>";
+for($i=1;$i<=5;$i++){
+    echo "ferie tuż tuż <br>";
+}
+echo $linia;
+
+for($a=1;$a<=50;$a++){
+echo "$a ";
+}
+echo $oba;
+
+for ($b=100;$b>=50;$b-=5){
+    echo "$b ";
+}
+
+?>
