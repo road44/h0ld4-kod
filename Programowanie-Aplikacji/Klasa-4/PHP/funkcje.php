3479ea7fa2f5516ba2b1ae484c7ea2af1611e736
road44 <52355930+road44@users.noreply.github.com>
2026-02-05 09:37:49 +0100

    1.6 (fun)

--- Programowanie-Aplikacji/Klasa-4/PHP/funkcje.php
+++ Programowanie-Aplikacji/Klasa-4/PHP/funkcje.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>funkcje</title>
+    <style>
+  
+    </style>
 </head>
 <body>
     
@@ -29,6 +32,60 @@ sumowanie(-12,45);
 
 echo $linia;
 // przykład 3. funkcja obliczająca rabat
+
+function rabat($kwota, $procent){
+    $obnizka = $kwota - ($kwota * $procent/100);
+    echo "$kwota po obniżce wynosi: $obnizka <br>";
+}
+
+rabat(100,10);
+rabat(2000, 44);
+echo $linia;
+
+
+function lancuch($znak, $dl){
+    echo "<ol>";
+    for($i=0;$i<$dl;$i++){
+        echo "<li>$znak </li>";
+        }
+        echo "</ol>";
+}
+        
+        
+lancuch("&", 7);
+
+echo $linia;
+
+function cwiartka($liczba){
+    echo $liczba / 4 . '<br>';
+}
+
+cwiartka(100);
+cwiartka(678);
+
+echo $linia;
+
+
+
+function co_piec(){
+    for($a=0;$a<=100;$a+=5){
+        echo "$a ";
+    }
+}
+
+co_piec();
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
 
 
