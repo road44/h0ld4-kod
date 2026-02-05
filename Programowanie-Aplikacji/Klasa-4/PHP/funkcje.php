83a3cee32c2597a328cd17168744300015572d82
road44 <52355930+road44@users.noreply.github.com>
2026-02-05 09:11:39 +0100

    1.4 (fun)

--- Programowanie-Aplikacji/Klasa-4/PHP/funkcje.php
+++ Programowanie-Aplikacji/Klasa-4/PHP/funkcje.php
@@ -20,11 +20,12 @@ hello();
 
 function sumowanie($a, $b){
     $suma = $a + $b;
-    echo "suma liczb wynosi: $suma";
+    echo "suma liczb wynosi: $suma <br>";
 }
 echo $pauza;
 sumowanie(12,10);
-
+sumowanie(5,12);
+sumowanie(-12,45);
 
 
 ?>
