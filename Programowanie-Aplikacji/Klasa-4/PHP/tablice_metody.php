f37685db9a9f627b8547fe94042d088a2eaafa85
road44 <52355930+road44@users.noreply.github.com>
2026-03-17 13:03:42 +0100

    tm 1.3

--- Programowanie-Aplikacji/Klasa-4/PHP/tablice_metody.php
+++ Programowanie-Aplikacji/Klasa-4/PHP/tablice_metody.php
@@ -2,22 +2,25 @@
 
 $x=['banan','kiwi','agrest'];
 
-$x[0]="mango";
+$x[0]="mango"; //zastepowanie
 print_r($x);
 echo "<hr>";
 
-$x[3]="papaja";
-$x[6]="jagoda";
+$x[3]="papaja"; //dodanie na 3 index
+$x[6]="jagoda"; //dodanie na 6 index
 print_r($x);
 echo "<hr>";
 
-array_push($x,'gruszka','jajko');
+// push doda element na koniec lub kilka
+array_push($x,'gruszka');
 print_r($x);
 echo "<hr>";
 
 
-
-
+// pop usuwa ostatni element
+array_pop($x);
+print_r($x);
+echo "<hr>";
 
 
 
