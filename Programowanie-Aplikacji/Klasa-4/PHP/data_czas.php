80c51b75708b8247c16742bf0080ed1aae8a24b7
road44 <52355930+road44@users.noreply.github.com>
2026-03-19 09:24:08 +0100

    ds 1.2

--- Programowanie-Aplikacji/Klasa-4/PHP/data_czas.php
+++ Programowanie-Aplikacji/Klasa-4/PHP/data_czas.php
@@ -1,8 +1,13 @@
 <?php
+$br = '<br>';
+echo time();
+echo $br;
 
-
-
-
+$data = getdate();
+$dzien = $data["mday"];
+$miesiac=$data['mon'];
+$rok = $data['year'];
+echo "Bierząca data to: $dzien-$miesiac-$rok r.";
 
 
 
