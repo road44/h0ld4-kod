2abac3c555a1f7fbbdcebfcbaa7e3dbf65a295ef
road44 <52355930+road44@users.noreply.github.com>
2026-03-25 09:35:52 +0100

    czas i data 2

--- Programowanie-Aplikacji/Klasa-4/PHP/data_czas.php
+++ Programowanie-Aplikacji/Klasa-4/PHP/data_czas.php
@@ -44,11 +44,11 @@ echo "dziś jest: ".$dnipl[date('l')].". ".date('d/m/Y').$br;
 
 
 
-$teraz = time();
-$zagodz=$teraz +3600;
-echo "Teraz $teraz, za godzine $zagodz $br";
-echo date("H:i", $zagodz);
-// data do przodu (jutro)
+// $teraz = time();
+// $zagodz=$teraz +3600;
+// echo "Teraz $teraz, za godzine $zagodz $br";
+// echo date("H:i", $zagodz);
+// // data do przodu (jutro)
 
 
 $d = date('d');
@@ -61,6 +61,27 @@ echo "$br jutro będzie $d/$m/$y $br";
 
 // określenie daty musi być wartością typu string
 // określenia: last/next/week/month/year/-2 weeks/ +7 days
+
+$teraz = strtotime("now");
+echo "$br Teraz: ";
+echo date('H:i:s', $teraz);
+
+$zatydzien = strtotime('next Wednesday');
+echo "$br Za tydzień: ";
+echo date("d.m.Y",$zatydzien)." mamy kartkówke";
+
+$roktemu=strtotime("1 year ago");
+echo "$br Rok temu: ";
+echo date('d.m.Y',$roktemu)." uczyliśmy sie JS";
+
+$zamies=strtotime('+30 days');
+echo $br;
+echo date('d.m.Y',$zamies)." organizujemy akadamie pożegnalną dla maturzystów";
+
+$wczesniej=strtotime('4 years ago');
+echo "$br w roku ";
+echo date("Y", $wczesniej)."Chodziliśmy do SP";
+
 
 
 echo '<hr><a href="index.php">Index</a>';
