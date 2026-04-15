8d4572c1ec3911c7453369397f21323249b09083
road44 <52355930+road44@users.noreply.github.com>
2026-04-15 09:35:43 +0200

    rw 1.11

--- Programowanie-Aplikacji/Klasa-4/PHP/regularne/reg3.php
+++ Programowanie-Aplikacji/Klasa-4/PHP/regularne/reg3.php
@@ -1,19 +1,23 @@
 <?php
 // imie i naswisko od wielkiej litery
-// mogą pojawić się polskie znaki
+// mogą pojawić się polskie znaki → flaka "u" 
 
-$wz = '/^[A-ZŁŻ]{1}[a-zółż]{2,}\s[A-ZŁŚĆŻŹĄŁ]{1}[a-zęóśżźćńł}{1,}$/';
-
-
-
+// $wz = '/^[A-ZŁŻ]{1}[a-zółż]{2,}\s[A-ZŁŚĆŻŹĄŁ]{1}[a-zęóśżźćńł]{1,}$/u';
+$wz='/^[ \pl -]+$/u';
 
 
+if(preg_match($wz,$_POST['dane'])){
+    echo "Poprawne dane";
+}
+else{
+    echo 'Błąð. Imie i nazwisko powinno zaczynać się od wielkiej litery';
+}
 
 
 
 
 
 echo '<hr>';
-echo '<a href="reg2.html">Powrót</a><br>';
+echo '<a href="reg3.html">Powrót</a><br>';
 echo '<a href="../index.php">Index</a>';
 ?>
