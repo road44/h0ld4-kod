e877536665dc0a052a0992ab0f21cf65c2d12855
road44 <52355930+road44@users.noreply.github.com>
2026-04-22 09:29:28 +0200

    reg4 1.2

--- Programowanie-Aplikacji/Klasa-4/PHP/regularne/reg4.php
+++ Programowanie-Aplikacji/Klasa-4/PHP/regularne/reg4.php
@@ -1,13 +1,32 @@
 <?php
 
+// wielkość liter nie ma znaczenia
+// +pl znaki
+
+$wz_im='/^[a-złżA-ZŁŻ]{2,}$/';
 
 
+if(!empty($_POST['imie']) && preg_match($wz_im, $_POST['imie'])){
+    echo "Imię to: ". $_POST['imie'];
+}
+else if(!empty($_POST['imie']) && !preg_match($wz_im, $_POST['imie'])){
+    echo "Błąd w imieniu";
+}
+else{
+    echo "uzupełnij pole imie";
+}
 
+// mail
 
-
-
-
-
+if(!empty($_POST['mail']) && filter_var($_POST['mail'],FILTER_VALIDATE_EMAIL)){
+    echo "<br> Email to " . $_POST['mail'];
+}
+else if(empty($_POST['mail'])){
+    echo "<br>Podaj adres email";
+}
+else{
+    echo "<br>Błąd w adresie email";
+}
 
 
 
