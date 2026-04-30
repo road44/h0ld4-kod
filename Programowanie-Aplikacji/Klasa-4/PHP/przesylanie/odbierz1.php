246cfb0fc914cc027a05ebe17480581707fa278a
road44 <52355930+road44@users.noreply.github.com>
2026-04-30 09:00:01 +0200

    p 1.3

--- Programowanie-Aplikacji/Klasa-4/PHP/przesylanie/odbierz1.php
+++ Programowanie-Aplikacji/Klasa-4/PHP/przesylanie/odbierz1.php
@@ -1,5 +1,15 @@
 <?php
+$b = '<br>';
+echo "<b>Informacje o pliku :<b>$b";
 
+echo $_FILES['plik1']['type'].$b;
 
+echo $_FILES['plik1']['size'].$b;
 
+echo $_FILES['plik1']['name'].$b;
+
+echo $_FILES['plik1']['tmp_name'].$b;
+
+echo $_FILES['plik1']['error'].$b;
+echo "<hr>";
 ?>
