77bb02a00de7239ec1164837570495f7e476caab
road44 <52355930+road44@users.noreply.github.com>
2026-06-02 13:09:16 +0200

    p3 1.6

--- Programowanie-Aplikacji/Klasa-4/PHP/przesylanie/odbierz3.php
+++ Programowanie-Aplikacji/Klasa-4/PHP/przesylanie/odbierz3.php
@@ -1,7 +1,22 @@
 <?php
 $b="<br>";
 
+function spr_typ(){
+    if(is_uploaded_file($_FILES['obrazek']['tmp_name'])){
+        if($_FILES['obrazek']['type']!="image/jpeg"){
+        echo "Nie właściwy format pliku $b";
+        }
+        else {
+            echo "Plik wysłano $b";
+        }
+    
+    }
+    else{
+        echo "Nie przesłano pliku $b";
+    }
+}
 
+spr_typ();
 
 
 
